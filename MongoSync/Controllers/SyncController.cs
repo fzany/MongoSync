a8d2ed2a49cbe28ffbe114acf570c8789f3dfba2
olorunfemi <oajibulu@outlook.com>
2022-05-27 10:31:32 +0100

    Draft

---
+++ MongoSync/Controllers/SyncController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoSync.Models;
+using MongoSync.Services;
+using Newtonsoft.Json;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Labs.Search;
+
+namespace MongoSync.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SyncController : ControllerBase
+    {
+        private readonly MongoService _mongo;
+        private readonly ILogger<SyncController> _logger;
+
+        public SyncController(ILogger<SyncController> logger, MongoService mongo)
+        {
+            _logger = logger;
+            _mongo = mongo;
+        }
+
+        [HttpGet("TestConnection")]
+        public async Task<ActionResult> TestConnection()
+        {
+            try
+            {
+
+                var count = await _mongo._users.CountDocumentsAsync(d => true);
+                return Ok(count);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetUsers")]
+        public async Task<ActionResult> GetUsers()
+        {
+            try
+            {
+                var users = await _mongo._users.Find(d => true).FirstOrDefaultAsync();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("RunSimulation/{query},{times}")]
+        public async Task<ActionResult<List<User>>> RunSimulation(string query, int times)
+        {
+            try
+            {
+                BsonDocument pipeline = new BsonDocument {
+  new BsonDocument("$search", new BsonDocument {
+    {
+      "index",
+      "default"
+    },
+    {
+      "text",
+      new BsonDocument {
+        {
+          "query",
+          query
+        },
+        {
+          "path",
+          new BsonDocument("wildcard", "*")
+        }
+      }
+    }
+  }) };
+
+              var result = await _mongo._usersBson.AggregateAsync(pipeline);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /*v
+}*/
+
+        [HttpGet("Upload")]
+        public async Task<ActionResult> Upload()
+        {
+            try
+            {
+                var file = await System.IO.File.ReadAllTextAsync("/Users/olorunfemi/Downloads/User.json");
+                var data = JsonConvert.DeserializeObject<List<User>>(file);
+
+                //Insert into the database.
+                await _mongo._users.InsertManyAsync(data);
+                return Ok("Done");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ MongoSync/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using MongoSync.Models;
+using MongoSync.Services;
+
+namespace MongoSync
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSwaggerGen(c =>
+            {
+                // Set Title and version
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Involved", Version = "v3", Description = "A middleware project that connects data sources for information exchange." });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                // pick comments from classes, including controller summary comments
+                c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+                // _OR_ enable the annotations on Controller classes [SwaggerTag], if no class comments present
+                c.EnableAnnotations();
+            });
+
+            services.Configure<ConnectionStrings>(Configuration.GetSection(nameof(ConnectionStrings)));
+
+            services.AddSingleton<MongoService>();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Involved R7D v1");
+                // c.InjectStylesheet("/swagger-ui/SwaggerDark.css");
+                c.RoutePrefix = string.Empty;
+            });
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
